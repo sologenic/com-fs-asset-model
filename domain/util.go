f398a6a3423122a866c625d8ef4f8f70b53499df
“jayjang0209” <jayjang0209@gmail.com>
2025-05-21 11:52:32 -0700

    ehn: add issuer to CreateAssetKeystr func

--- domain/util.go
+++ domain/util.go
@@ -6,7 +6,7 @@ import (
 	assetdmncurrency "github.com/sologenic/com-fs-asset-model/domain/currency"
 )
 
-// Asset key format: {currency}_{organizationID}, {symbol}_{version}_{organizationID}
-func CreateAssetKeyStr(currency *assetdmncurrency.Currency, organizationID string) string {
-	return fmt.Sprintf("%s_%s", currency.ToString(), organizationID)
+// Asset key format: {currency}_{organizationID}_{issuer}, {symbol}_{version}_{organizationID}_{issuer}
+func CreateAssetKeyStr(currency *assetdmncurrency.Currency, organizationID, issuer string) string {
+	return fmt.Sprintf("%s_%s_%s", currency.ToString(), organizationID, issuer)
 }
